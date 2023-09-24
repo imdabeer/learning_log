53ef6f8c2ba29a17ce81386950fd253fd3ad09b2
imdabeer <imdabeerdanial@gmail.com>
2023-09-24 14:22:54 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pnq)%z%u%_-%cwb)f*8osy84pck*^as_l)*t$mks(d^o^k2vk)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
