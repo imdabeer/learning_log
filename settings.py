1c0badf9917ef5e721400ca6f956338fc3b9490e
imdabeer <imdabeerdanial@gmail.com>
2023-09-29 11:49:51 +0500

    Updated debug to true

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pnq)%z%u%_-%cwb)f*8osy84pck*^as_l)*t$mks(d^o^k2vk)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
